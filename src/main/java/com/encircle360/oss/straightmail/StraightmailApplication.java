ccfbf4d32c529a9b05523afa24f512b4eaf5de3b
Marcello Muscara <marcello@codeministry.de>
2021-02-10 13:03:20 +0100

    Fixed messageSource.setBasenames() includes classpath AND external resource folder now

--- src/main/java/com/encircle360/oss/straightmail/StraightmailApplication.java
+++ src/main/java/com/encircle360/oss/straightmail/StraightmailApplication.java
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
@@ -14,6 +15,7 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 
 import com.encircle360.oss.straightmail.wrapper.JsonNodeObjectWrapper;
 
+@Slf4j
 @SpringBootApplication
 public class StraightmailApplication {
 
@@ -24,12 +26,9 @@ public class StraightmailApplication {
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        if (Files.exists(Path.of("file:/resources/i18n/messages"))) {
-            messageSource.setBasenames("file:/resources/i18n/messages", "i18n/messages");
-        } else {
-            messageSource.setBasename("i18n/messages");
-        }
+        messageSource.setBasenames("classpath:i18n/messages", "file:/resources/i18n/messages");
         messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+
         return messageSource;
     }
 
