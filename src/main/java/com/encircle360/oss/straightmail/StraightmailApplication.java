550748c86b4d5a677117c73c9f6750b801d352b7
peter <ps@web-eifel.de>
2021-02-23 14:14:51 +0100

    added mongodb, but disabled on default

--- src/main/java/com/encircle360/oss/straightmail/StraightmailApplication.java
+++ src/main/java/com/encircle360/oss/straightmail/StraightmailApplication.java
@@ -3,20 +3,28 @@ package com.encircle360.oss.straightmail;
 import static freemarker.template.Configuration.VERSION_2_3_28;
 
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 import com.encircle360.oss.straightmail.wrapper.JsonNodeObjectWrapper;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    MongoAutoConfiguration.class,
+    MongoDataAutoConfiguration.class,
+    DataSourceAutoConfiguration.class,
+    MongoRepositoriesAutoConfiguration.class
+})
 public class StraightmailApplication {
 
     public static void main(String[] args) {
