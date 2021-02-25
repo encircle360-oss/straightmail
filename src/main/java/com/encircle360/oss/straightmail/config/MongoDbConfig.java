cd6faa91478216c16419b6f78a414c5d1e7e76e3
peter <ps@web-eifel.de>
2021-02-25 13:57:51 +0100

    added correct mongodb configuration

--- src/main/java/com/encircle360/oss/straightmail/config/MongoDbConfig.java
+++ src/main/java/com/encircle360/oss/straightmail/config/MongoDbConfig.java
@@ -10,11 +10,20 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @Profile({PROFILE})
+@RequiredArgsConstructor
 @EnableConfigurationProperties(MongoProperties.class)
 @EnableMongoRepositories("com.encircle360.oss.straightmail.repository")
 public class MongoDbConfig extends AbstractMongoClientConfiguration {
+
+    private final MongoProperties properties;
 
     public final static String PROFILE = "mongo";
 
@@ -25,4 +34,21 @@ public class MongoDbConfig extends AbstractMongoClientConfiguration {
     protected String getDatabaseName() {
         return database;
     }
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        String uri = properties.getUri();
+        if (uri == null) {
+            uri = "mongodb://" + properties.getHost() + ":" + properties.getPort() + "/" + properties.getDatabase();
+        }
+
+        ConnectionString connectionString = new ConnectionString(uri);
+        builder.applyConnectionString(connectionString);
+
+        if (properties.getUsername() != null && properties.getAuthenticationDatabase() != null && properties.getPassword() != null) {
+            MongoCredential credentials = MongoCredential.createCredential(properties.getUsername(), properties.getAuthenticationDatabase(), properties.getPassword());
+            builder.credential(credentials);
+        }
+    }
+
 }
